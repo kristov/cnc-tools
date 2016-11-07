0fe3e0a2ee958e685abdf480ea8bd716653f21bf
Chris Eade <chris.eade@gmail.com>
2016-11-07 09:08:47 +0100

    Trying to separate programs out a bit

---
+++ lib/CNCTool/Program/SliceModel.pm
@@ -0,0 +1,190 @@
+package CNCTool::Program::SliceModel;
+
+use Moose;
+use File::Path qw( make_path );
+use CNCTool::Slice;
+use CNCTool::Logger;
+
+sub options {
+    return [
+        {
+            name    => 'stl_file',
+            type    => 'file',
+            req     => 1,
+            desc    => 'STL file to slice',
+        },
+        {
+            name    => 'steps_per_mm',
+            type    => 'decimal',
+            req     => 0,
+            desc    => 'Number of steps per mm of machine',
+        }
+    ];
+}
+
+has project => (
+    is => 'ro',
+    isa => 'CNCTool::Project',
+    required => 1,
+);
+
+has stl_file => (
+    is => 'ro',
+    isa => 'Str',
+    required => 1,
+);
+
+has steps_per_mm => (
+    is => 'ro',
+    isa => 'Num',
+    required => 0,
+);
+
+has layer_files => (
+    is => 'ro',
+    isa => 'ArrayRef[Str]',
+    lazy => 1,
+    builder => '_build_layer_files',
+);
+
+sub _build_layer_files {
+    my ( $self ) = @_;
+
+    my $raw_layer_files_dir = $self->raw_layer_files_dir;
+
+    if ( -d $raw_layer_files_dir ) {
+        opendir( my $dfh, $raw_layer_files_dir ) || die "reading $raw_layer_files_dir: $!";
+        my @files = readdir( $dfh );
+        closedir( $dfh );
+        @files = grep { $_ =~ /^[0-9]+\.layer/ } @files;
+        return \@files;
+    }
+    return [];
+}
+
+has nr_layers => (
+    is => 'rw',
+    isa => 'Int',
+    lazy => 1,
+    builder => '_build_nr_layers',
+);
+
+sub _build_nr_layers {
+    my ( $self ) = @_;
+    my $layer_files = $self->layer_files;
+    return scalar( @{ $layer_files } );
+}
+
+has min_travel => (
+    is => 'ro',
+    isa => 'Num',
+    required => 0,
+    builder => '_build_min_travel',
+);
+
+sub _build_min_travel {
+    my ( $self ) = @_;
+    my $steps_per_mm = $self->steps_per_mm // 314.961;
+    return sprintf( '%0.4f', 1 / $steps_per_mm );
+}
+
+sub raw_layer_files_dir {
+    my ( $self ) = @_;
+    return $self->project->generate_dir( 'raw_layer_files' );
+}
+
+sub run {
+    my ( $self ) = @_;
+    $self->project->make_project_dir( 'raw_layer_files' );
+    $self->slice_model;
+}
+
+sub slice_model {
+    my ( $self ) = @_;
+
+    my $slicer = CNCTool::Slice->new( { file => $self->stl_file } );
+
+    my $min_travel = $self->min_travel;
+    my $max_height = $slicer->max_height;
+    my $min_height = $slicer->min_height;
+
+    my @z_heights;
+    for ( my $z = $max_height; $z > $min_height; $z -= $min_travel ) {
+        push @z_heights, sprintf( '%0.4f', $z );
+    }
+
+    my $nr_layers = scalar( @z_heights );
+    $self->nr_layers( $nr_layers );
+    $self->project->logger->debug( 'calculated %d layers to slice', $nr_layers );
+
+    $self->_slice_multi_process( $slicer, @z_heights );
+}
+
+sub _slice_single_process {
+    my ( $self, $slicer, @z_heights ) = @_;
+
+    for my $z ( @z_heights ) {
+        $self->project->logger->debug( 'slicing at %0.4f', $z );
+        my $layer = $slicer->layer_at_z( $z );
+        $self->save_layer( $z, $layer );
+    }
+}
+
+sub _slice_multi_process {
+    my ( $self, $slicer, @z_heights ) = @_;
+
+    my $cpu_count = $self->_cpu_count;
+    $self->project->logger->debug( 'detected %d CPUs', $cpu_count );
+
+    require Parallel::ForkManager;
+    my $pm = Parallel::ForkManager->new( $cpu_count );
+
+    DATA_LOOP: for my $z ( @z_heights ) {
+        my $pid = $pm->start and next DATA_LOOP;
+
+        $self->project->logger->debug( 'PID %d: slicing at %0.4f', $$, $z );
+        my $layer = $slicer->layer_at_z( $z );
+        $self->save_layer( $z, $layer );
+
+        $pm->finish;
+    }
+}
+
+sub _cpu_count {
+    my ( $self ) = @_;
+    my $cpu_count = `grep -c -P '^processor\\s+:' /proc/cpuinfo`;
+    chomp $cpu_count;
+    return $cpu_count;
+}
+
+sub unique_layers {
+    my ( $self ) = @_;
+    my $layer_files = $self->layer_files;
+
+    for my $file ( @{ $layer_files }[0..5] ) {
+        if ( $file =~ /^([0-9]+)\./ ) {
+            my $data = $self->load_layer( $1 + 0 );
+            print Dumper( $data );
+        }
+    }
+}
+
+sub save_layer {
+    my ( $self, $ident, $layer ) = @_;
+
+    my $file_name = sprintf( '%0.5f.layer', $ident );
+
+    my $content = $self->project->serialise( $layer );
+    $self->project->save_file( 'raw_layer_files', $file_name, $content );
+}
+
+sub load_layer {
+    my ( $self, $count ) = @_;
+
+    my $file_name = sprintf( '%05d.layer', $count );
+
+    my $content = $self->project->load_file( 'raw_layer_files', $file_name );
+    return $self->project->deserialise( $content );
+}
+
+__PACKAGE__->meta->make_immutable;
--- lib/CNCTool/Project.pm
+++ lib/CNCTool/Project.pm
@@ -4,44 +4,76 @@ use Moose;
 use File::Path qw( make_path );
 use CNCTool::Slice;
 use CNCTool::Logger;
+use Getopt::Long;
+use CNCTool::Program::SliceView;
+use CNCTool::Program::SliceModel;
 
-has project => (
+has project_name => (
+    is => 'rw',
+    isa => 'Str',
+    required => 0,
+);
+
+has program_name => (
     is => 'ro',
     isa => 'Str',
     required => 1,
 );
 
-has stl => (
+has program_class => (
     is => 'ro',
-    isa => 'Str',
-    required => 1,
+    isa => 'HashRef',
+    default => sub {
+        return {
+            'slice-model' => 'CNCTool::Program::SliceModel',
+            'slice-view'  => 'CNCTool::Program::SliceView',
+        };
+    },
 );
+
+has program => (
+    is => 'rw',
+    isa => 'Object',
+);
+
+sub get_options {
+    my ( $self, $options ) = @_;
+
+    my @def = @{ $options || [] };
+
+    my @opts = (
+        'project=s',
+        'help',
+    );
+
+    for my $def ( @def ) {
+        if ( $def->{type} eq 'bool' ) {
+            push @opts, $def->{name};
+        }
+        else {
+            push @opts, $def->{name} . '=s';
+        }
+    }
+
+    my $args = {};
+    GetOptions(
+        $args,
+        @opts,
+    );
+
+    if ( $args->{help} ) {
+        $self->print_help;
+    }
+
+    return $args;
+}
 
 has project_root => (
     is => 'ro',
     isa => 'Str',
-    default => './',
+    default => '.',
     required => 0,
 );
-
-has steps_per_mm => (
-    is => 'ro',
-    isa => 'Num',
-    required => 0,
-);
-
-has min_travel => (
-    is => 'ro',
-    isa => 'Num',
-    required => 0,
-    builder => '_build_min_travel',
-);
-
-sub _build_min_travel {
-    my ( $self ) = @_;
-    my $steps_per_mm = $self->steps_per_mm // 314.961;
-    return sprintf( '%0.4f', 1 / $steps_per_mm );
-}
 
 has logger => (
     is => 'ro',
@@ -57,13 +89,28 @@ sub _build_logger {
 
 sub BUILD {
     my ( $self ) = @_;
+
+    my $program_name = $self->program_name;
+
+    my $program_class = $self->program_class->{$program_name}
+        || die "unknown program_name: $program_name";
+
+    my $options = $program_class->options;
+    my $args = $self->get_options( $options );
+
+    $self->project_name( delete $args->{project} );
+
+    my $program_object = $program_class->new( { project => $self, %{ $args } } );
+
+    $self->program( $program_object );
+
     $self->make_project_dir;
 }
 
 sub generate_dir {
     my ( $self, @path ) = @_;
 
-    my $dir = sprintf( '%s/projects/%s', $self->project_root, $self->project );
+    my $dir = sprintf( '%s/projects/%s', $self->project_root, $self->project_name );
     if ( @path ) {
         $dir .= '/' . join( '/', @path );
     }
@@ -77,41 +124,17 @@ sub make_project_dir {
     make_path( $dir );
 }
 
-sub slice_model {
-    my ( $self ) = @_;
-
-    $self->make_project_dir( 'raw_layer_files' );
-
-    my $slicer = CNCTool::Slice->new( { file => $self->stl } );
-
-    my $min_travel = $self->min_travel;
-    my $max_height = $slicer->max_height;
-    my $min_height = $slicer->min_height;
-
-    my @z_heights;
-    for ( my $z = $max_height; $z > $min_height; $z -= $min_travel ) {
-        push @z_heights, sprintf( '%0.4f', $z );
-    }
-
-    $self->logger->debug( 'calculated %d layers to slice', scalar( @z_heights ) );
-
-    my $count = 0;
-    for my $z ( @z_heights ) {
-        $self->logger->debug( 'slicing at %0.4f', $z );
-        my $layer = $slicer->layer_at_z( $z );
-        $self->save_layer( $count, $layer );
-        $count++;
-    }
+sub serialise {
+    my ( $self, $layer ) = @_;
+    use Data::Dumper;
+    return Data::Dumper->new( [ $layer ], [ 'data' ] )->Indent( 1 )->Dump;
 }
 
-sub save_layer {
-    my ( $self, $count, $layer ) = @_;
-
-use Data::Dumper;
-    my $file_name = sprintf( '%05d.layer', $count );
-    my $content = Data::Dumper->new( [ $layer ], [ 'layer' ] )->Indent( 1 )->Dump;
-
-    $self->save_file( 'raw_layer_files', $file_name, $content );
+sub deserialise {
+    my ( $self, $content ) = @_;
+    my $data;
+    eval "$content";
+    return $data;
 }
 
 sub save_file {
@@ -123,6 +146,21 @@ sub save_file {
     open( my $fh, '>', $name ) || die "opening: $name: $!";
     print $fh $content;
     close $fh;
+}
+
+sub load_file {
+    my ( $self, $subdir, $file_name ) = @_;
+
+    my $dir = $self->generate_dir( $subdir );
+    my $name = $dir . '/' . $file_name;
+
+    my $content;
+    open( my $fh, '<', $name ) || die "opening: $name: $!";
+    while ( <$fh> ) {
+        $content .= $_;
+    }
+    close $fh;
+    return $content;
 }
 
 __PACKAGE__->meta->make_immutable;
